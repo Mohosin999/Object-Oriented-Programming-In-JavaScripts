595467de6d4aee12acf61415e4ed73aa853989e0
Mohosin999 <mohosin.h.akash@gmail.com>
2023-09-30 14:39:16 +0600

    add some method into class

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 class Person {
+  // "this" keyword means full object
+  // When you'll work with "this", ignore arrow function
   constructor(name, email) {
     this.name = name;
     this.email = email;
   }
+
+  changeName(name) {
+    this.name = name;
+  }
+
+  sendEmail(msg) {
+    console.log(`sending email ${msg}`);
+  }
 }
 
-const p1 = new Person("akash", "test@test.com");
-const p2 = new Person("akash hasan", "akash@gmail.com");
+const p1 = new Person("akash", "akash@test.com");
+const p2 = new Person("mohosin", "mohosin@test.com");
 
 console.log(p1);
 console.log(p2);
+
+p1.changeName("Mohosin hasan Akash");
+console.log(p1);
